6db5a183fac856c2974467cdf0829a7376307988
Matej <hozlarmatej0@gmail.com>
2025-04-24 16:02:22 +0200

    dev environment fixes

--- client/src/components/ApplyToJoin.jsx
+++ client/src/components/ApplyToJoin.jsx
@@ -35,7 +35,7 @@ const ApplyToJoin = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/apply", {
+      const response = await fetch("http://localhost:5000/apply", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
--- client/src/components/WaitlistNotice.jsx
+++ client/src/components/WaitlistNotice.jsx
@@ -11,7 +11,7 @@ const WaitlistNotice = () => {
       return;
     }
     try {
-      const response = await fetch("/api/wait-list", {
+      const response = await fetch("http://localhost:5000/wait-list", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
