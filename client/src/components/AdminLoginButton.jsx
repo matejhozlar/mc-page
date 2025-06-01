0a14a96f75da4467900cac956b98d03bcbf5b563
Matej <hozlarmatej0@gmail.com>
2025-06-01 17:50:50 +0200

    reverting issues

--- client/src/components/AdminLoginButton.jsx
+++ client/src/components/AdminLoginButton.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const AdminLoginButton = () => {
   const handleLogin = () => {
     const clientId = "1367925825420267565";
-    const redirectUri = encodeURIComponent(
-      "https://create-rington.com/callback"
-    );
+    const redirectUri = encodeURIComponent("http://localhost:3000/callback");
     const scope = "identify";
     const discordUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&prompt=consent`;
     window.location.href = discordUrl;
