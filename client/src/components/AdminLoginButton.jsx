8b6ad51241a816f0a7fe6fc8e6a6d7b0ef1f22de
Matej <hozlarmatej0@gmail.com>
2025-06-01 17:49:16 +0200

    client init

--- client/src/components/AdminLoginButton.jsx
+++ client/src/components/AdminLoginButton.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const AdminLoginButton = () => {
   const handleLogin = () => {
     const clientId = "1367925825420267565";
-    const redirectUri = encodeURIComponent("http://localhost:3000/callback");
+    const redirectUri = encodeURIComponent(
+      "https://create-rington.com/callback"
+    );
     const scope = "identify";
     const discordUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&prompt=consent`;
     window.location.href = discordUrl;
