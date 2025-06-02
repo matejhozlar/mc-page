1985387c9dd54efca127091061eb81ec1d8b7146
Matej <hozlarmatej0@gmail.com>
2025-06-02 17:05:37 +0200

    updating channel log

--- server/bin/announcements/announceChannelLog.js
+++ server/bin/announcements/announceChannelLog.js
@@ -18,48 +18,27 @@ client.once("ready", async () => {
     );
 
     const embed = new EmbedBuilder()
-      .setTitle("🛠️ Createrington v0.1.6 Modpack Update")
+      .setTitle("🛠️ Createrington v0.1.7 Modpack Update")
       .setColor(0x00b0f4)
       .setDescription(
-        "A new version of the modpack is now available! Please update to **v0.1.6** to enjoy new content and improvements."
+        "A new version of the modpack is now available! Please update to **v0.1.7** to enjoy new content and improvements."
       )
       .addFields(
         {
           name: "🆕 New Mods",
           value: [
-            "- Create: Aquatic Ambitions",
-            "- Create: Connected",
-            "- Create: Railways Navigator",
-            "- Create: Stock Bridge",
-            "- Create: Central Kitchen",
-            "- The Afterdark",
-            "- Deeper Darker",
-            "- Jamlib",
-            "- KubeJS",
-            "- Rhino",
-            "- ResourcefulConfig",
-            "- RightClickHarvest",
-            "- Tempad",
-          ].join("\n"),
-        },
-        {
-          name: "⬆️ Updated Mods",
-          value: [
-            "- Create: Dragons Plus",
-            "- Create: Enchantment Industry",
-            "- Create: Garnished",
-            "- Txnlib",
+            "- AdvancedAE",
+            "- ExtendedAE",
+            "- Createrington Currency",
+            "- Create: Copycats +",
+            "- Create: Deep Dark",
+            "- Geckolib",
           ].join("\n"),
         },
         {
           name: "📢 Reminder",
           value:
             "Please **update the modpack** to the latest version.\nIf you encounter **any bugs or issues**, report them as soon as possible!",
-        },
-        {
-          name: "⚠️ IMPORTANT: Teleportation Change",
-          value:
-            "**The `/tpa` command has been removed.**\nFrom now on, use the **Tempad** mod for any teleportation!\n(/home is still available)",
         }
       )
       .setFooter({ text: "Thanks for playing on Createrington!" })
