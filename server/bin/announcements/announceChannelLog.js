a9674599a64591f9c04658e8ea299218f592cadb
Matej <hozlarmatej0@gmail.com>
2025-09-18 17:06:01 +0200

    guide update

--- server/bin/announcements/announceChannelLog.js
+++ server/bin/announcements/announceChannelLog.js
@@ -7,13 +7,15 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
-const { CLIENT_BOT_TOKEN, DISCORD_TEST_CHANNEL_ID } = process.env;
+const { CLIENT_BOT_TOKEN, DISCORD_ANNOUNCEMENT_CHANNEL_ID } = process.env;
 
 client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
 
   try {
-    const channel = await client.channels.fetch(DISCORD_TEST_CHANNEL_ID);
+    const channel = await client.channels.fetch(
+      DISCORD_ANNOUNCEMENT_CHANNEL_ID
+    );
 
     const embed = new EmbedBuilder()
       .setTitle("🛠️ Createrington: Cogs & Steam v0.1.6a Modpack Update")
