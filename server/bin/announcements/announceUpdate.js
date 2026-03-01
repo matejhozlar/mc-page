8fe7129aa5fcfaf2b52e85968f11b907dfb8b637
Matej <hozlarmatej0@gmail.com>
2026-03-01 13:22:41 +0100

    npm audit

--- server/bin/announcements/announceUpdate.js
+++ server/bin/announcements/announceUpdate.js
@@ -13,7 +13,7 @@ const announcement = {
   title: "🔧 Server Maintenance",
   description:
     "We’re rolling out a modpack and server update to improve stability, performance and add new mods",
-  startsAt: new Date("2026-01-24T16:00:00+02:00"),
+  startsAt: new Date("2026-03-01T17:00:00+02:00"),
   estimatedMinutes: 120,
 };
 
